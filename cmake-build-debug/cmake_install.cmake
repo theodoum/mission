1f2533626e88c2bc46d9d2dd810588a0f24b7146
Boris CATHERINE <boris.catherine@gmail.com>
2023-06-29 13:53:17 +0200

    update main

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/boris/Desktop/Formation AJC/Programmation C++/git/mission
+# Install script for directory: C:/Users/boris/Desktop/Formation AJC/Programmation C++/Personnage/git/mission
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/boris/Desktop/Formation AJC/Programmation C++/git/mission/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/boris/Desktop/Formation AJC/Programmation C++/Personnage/git/mission/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <limits>
+
 #include "include/Controller.h"
 
 using namespace std;
@@ -12,16 +14,55 @@ void displayMainMenu()
     cout << "   0. Quitter le jeu" << endl;
 }
 
-int main() {
+int validInput()
+{
+    unsigned int num;
+    while (1) {
+        cin >> num;
+        if (cin.fail() && num > 3) {
+            cin.clear();
+            cin.sync();
+            cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
+            cout << "Erreur de saisie\n";
+            displayMainMenu();
+            continue;
+        }
+        return num;
+    }
+}
+
+int main()
+{
     Controller controller("test");
     controller.initialize();
-    controller.displayPlanets();
-    controller.displaySpaceships();
-    controller.displayCharacters();
-    controller.findPlanet("Terre")->getResidents().at(1)->setLife(124);
-    controller.displayCharacters();
-    controller.displayMissions();
-    controller.setSaveFile("save2.txt");
-    controller.save();
+//    controller.displayPlanets();
+//    controller.displaySpaceships();
+//    controller.displayCharacters();
+//    controller.findPlanet("Terre")->getResidents().at(1)->setLife(124);
+//    controller.displayCharacters();
+//    controller.displayMissions();
+   // controller.setSaveFile("save2.txt");
+    //controller.save();
+    while(1)
+    {
+        displayMainMenu();
+        int choice = validInput();
+        switch (choice) {
+            case 1:
+                controller.displayMissions();
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 0:
+                cout << u8"Où voulez-vous sauvegrder votre partie ? Appuyez sur Entrée pour utiliser le fichier de sauvegarde par défaut.\n";
+                string input;
+                cin >> input;
+                if (input.length()!=0) controller.setSaveFile(input);
+                controller.save();
+                return EXIT_SUCCESS;
+        }
+    }
     return 0;
 }
